b28db12a5094d3ad6f2544e45a7ad4aeb1995052
Travis Geis <ottobonn28@gmail.com>
2016-11-24 09:30:46 -0800

    Add ability to overlay a transparent title bar with PageFrame

--- components/PageFrame.js
+++ components/PageFrame.js
@@ -8,9 +8,10 @@ import { Ionicons } from "@exponent/vector-icons";
 
 export default class PageFrame extends Component {
   render() {
+    overlayStyle = this.props.overlay ? styles.overlay : null;
     return (
       <View style={{flex: 1}}>
-        <View style={styles.titleBar}>
+        <View style={[styles.titleBar, overlayStyle]}>
           <Ionicons name="md-arrow-round-back" size={32} color="white" />
           <Text style={styles.title}>{this.props.title}</Text>
         </View>
@@ -22,14 +23,28 @@ export default class PageFrame extends Component {
   }
 };
 
+PageFrame.propTypes = {
+  /* True if the frame should sit over the content */
+  overlay: React.PropTypes.bool,
+  /* The title to display in the title bar */
+  title: React.PropTypes.string
+};
+
+var titleBarHeight = 60;
 const styles = StyleSheet.create({
   titleBar: {
-    height: 60,
+    height: titleBarHeight,
     padding: 10,
     backgroundColor: "#4b4b4b",
     flexDirection: "row",
     alignItems: "center",
     elevation: 10
+  },
+  overlay: {
+    marginBottom: -titleBarHeight,
+    backgroundColor: "transparent",
+    elevation: 0, // Remove drop shadow
+    zIndex: 1000 // Make sure overlay is on top of children
   },
   title: {
     color: "white",
