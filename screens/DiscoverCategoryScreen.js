c935404f6387120920d9d1efeb0b3dfbbb3a5f09
Guhan Venkataraman <Guhan@DN51ul1f.SUNet>
2016-11-28 19:34:46 -0800

    added basic search bar

--- screens/DiscoverCategoryScreen.js
+++ screens/DiscoverCategoryScreen.js
@@ -1,17 +1,18 @@
 import React, {Component} from "react";
 import {
   View,
-  Text
+  Text,
+  TouchableOpacity,
 } from "react-native";
 
 import PageFrame from "../components/PageFrame";
 import EventListView from "../components/EventListView";
-
+import SearchBar from 'react-native-searchbar';
 import events from "../static/content/events.js";
 
 var eventList = [];
 
-export default class DiscoverCategoryScreen extends Component {
+export class DiscoverCategoryScreen extends Component {
 
   componentWillMount() {
     categoryName = this.props.route.params.name.toLowerCase();
@@ -36,3 +37,49 @@ export default class DiscoverCategoryScreen extends Component {
     );
   }
 };
+
+export default class SearchContainer extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      events,
+      results: []
+    };
+    this._handleResults = this._handleResults.bind(this);
+  }
+
+  _handleResults(results) {
+    this.setState({ results });
+  }
+
+  render() {
+    return (
+      <View>
+        <View style={{ marginTop: 110 }}>
+          {
+            this.state.results.map((result, i) => {
+              return (
+                <Text key={i}>
+                  {typeof result === 'object' && !(result instanceof Array) ? result.title : result.name}
+                </Text>
+              );
+            })
+          }
+          <TouchableOpacity onPress={() => this.searchBar.show()}>
+            <View style={{ backgroundColor: 'green', height: 100, marginTop: 50 }}/>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => this.searchBar.hide()}>
+            <View style={{ backgroundColor: 'red', height: 100 }}/>
+          </TouchableOpacity>
+        </View>
+        <SearchBar
+          ref={(ref) => this.searchBar = ref}
+          data={events}
+          handleResults={this._handleResults}
+          showOnLoad
+        />
+      </View>
+    );
+  }
+}
