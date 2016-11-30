205535d3723b96163f74e4d18a3dd3a5fa84dcf9
Travis Geis <ottobonn28@gmail.com>
2016-11-30 10:44:12 -0800

    Add sample data example to memories screen for UI dev

--- screens/MemoriesScreen.js
+++ screens/MemoriesScreen.js
@@ -3,6 +3,22 @@ import {
   Text
 } from "react-native";
 
+var memory = {
+  eventName: "Tree planting",
+  eventDate: Date(),
+  image: require("../static/content/images/memories/01.jpg")
+};
+
+var userData = {
+  name: "Sara",
+  image: require("../static/content/images/people/users/01_sara_douglas.jpg"),
+  memories: []
+};
+
+for (var i = 0; i < 10; i++) {
+  userData.memories.push(memory);
+}
+
 export default class MemoriesScreen extends React.Component {
   render() {
     return (
