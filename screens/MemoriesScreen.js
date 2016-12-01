91ac025c53e7abb61b79a3aacae2f44efa251997
Travis Geis <ottobonn28@gmail.com>
2016-12-01 11:59:42 -0800

    Wire memories to store

--- screens/MemoriesScreen.js
+++ screens/MemoriesScreen.js
@@ -5,35 +5,26 @@ import {
   StyleSheet,
   Image
 } from "react-native";
+import {connect} from "react-redux";
+
 import PageFrame from "../components/PageFrame";
 import MemoriesView from "../components/MemoriesView";
 import ChatHead from "../components/ChatHead";
 
-function randomDate(start, end) {
-  return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
-}
-
-var userData = {
-  name: "Travis",
-  image: require("../static/images/travis.jpg"),
-  memories: []
-};
-
-for (var i = 0; i < 50; i++) {
-  var memory = {
-  	eventName: "Tree planting",
-  	eventDate: randomDate(new Date(2016, 0, 10), new Date(2016, 11, 10)).toISOString(),
-  	image: require("../static/images/memories/01.jpg"),
-  };
-  userData.memories.push(memory);
-}
-
-export default class MemoriesScreen extends React.Component {
+class MemoriesScreen extends React.Component {
   render(){
     return (
       <PageFrame title="Memories" backButton={false} searchButton={false}>
-        <MemoriesView userData={userData} />
+        <MemoriesView userData={this.props.userData} />
       </PageFrame>
     );
   }
 }
+
+const mapStoreToProps = function(store) {
+  return {
+    userData: store.user
+  };
+};
+
+export default connect(mapStoreToProps)(MemoriesScreen);
--- store.js
+++ store.js
@@ -22,8 +22,27 @@ const eventsReducer = function(eventsState = [], action) {
   return newState;
 };
 
+const randomDate = function(start, end) {
+  return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
+};
+
+const userData = {
+  name: "Travis",
+  image: require("./static/images/travis.jpg"),
+  memories: []
+};
+
+for (var i = 0; i < 50; i++) {
+  var memory = {
+  	eventName: "Tree planting",
+  	eventDate: randomDate(new Date(2016, 0, 10), new Date(2016, 11, 10)).toISOString(),
+  	image: require("./static/images/memories/01.jpg"),
+  };
+  userData.memories.push(memory);
+}
+
 const userReducer = function(state, action) {
-  return {};
+  return userData;
 };
 
 const reducers = combineReducers({
